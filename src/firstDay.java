9290c2f38ad7d17ed97a4da7fbd88083882defed
Anibaba4296 <regiusdeveloperclub@gmail.com>
2023-06-06 11:16:08 +0530

    Day1

---
+++ src/firstDay.java
@@ -0,0 +1,13 @@
+//write the package name on the top
+// in this file we are not inside any package so we do not write here
+import java.util.Scanner;
+public class firstDay {
+    public static void main(String[] args){
+        System.out.println("Namaste duniya ");
+        System.out.print("Namaste Mayur bhai............");
+        System.out.println("Namaste Gaurav bhai");
+        Scanner sc = new Scanner(System.in);
+        byte var1 = sc.nextByte();
+        System.out.println(var1);
+    }
+}
